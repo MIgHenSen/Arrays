8a3d234a36609b6838ef53088cb63a777ca5c186
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-11-25 08:07:33 -0300

    Update Bubble_Sort.java

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -3,37 +3,25 @@ package exers;
  * de elementos adjacentes e, em seguida, trocar suas posicoes se estiverem na
  * ordem errada */
 
-/* Biblioteca (library) é uma coleção de subprogramas, que contem dados
- * auxiliares. Chamamos essas bibliotecas através dos imports */
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Bubble_Sort {
 	public static void main(String[] args) {
-		// Declaracao de variaveis do tipo inteiro
 		int tamanho, total=0, aux=0;
-		// Declaracao de scanner, para que o codigo leia o que o usuario digitar
 		Scanner scanner = new Scanner(System.in);
 		
-		/* As clausulas try/catch garantem o tratamento de excecoes, codigos 
-		 * que nao sao totalmente atendidos e geram algum erro */
 		try {
-			// Pequeno texto sobre a funcao do software
 			System.out.println("Organizando numeros em ordem crescente ");
 			System.out.println("Quantos nÃºmeros deseja organizar: ");
 			
-			//Variavel auxiliar para o tamanho da lista
 			tamanho = scanner.nextInt();
-			// Criacao da lista
 			int[] soma=new int[tamanho];
 			for(int i=0; i<tamanho; i++) {
-				// Pedido para informar os valores da lista, digitados pelo usuario
 				System.out.printf("Informe os numeros[%d]: ",i);
-				// Le os valores e os coloca em suas respectivas posicoes na lista
 				soma[i]=scanner.nextInt();
 			}
 			
-			// Metodo que oraganiza a lista
 			for(int i=0; i<tamanho; i++) {
 				for(int j=0; j<tamanho-1; j++) {
 					if(soma[j]>soma[j+1]) {
@@ -44,13 +32,11 @@ public class Bubble_Sort {
 				}
 			}
 			
-			// Agora, sao exibidos os valores da lista de forma organizada
 			System.out.println("Agora organizado: ");
 			for(int i=0; i<tamanho; i++) {
 				System.out.println(""+soma[i]);
 			}
 			
-		// Tratamento de excecoes
 		/* InputMismatchException indiga que um elemento solicitado nao existe, 
 		 * ou seja, o usuario digitou algo que nao deveria */
 		}catch(InputMismatchException e) {
